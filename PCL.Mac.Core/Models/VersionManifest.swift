cfb474d3c0790a536d1d77ad0c3a402da6dfe4a4
YiZhiMCQiu <yizhimcqiu@icloud.com>
2025-12-02 11:44:35 +0800

    feat: VersionManifest 数据模型

---
+++ PCL.Mac.Core/Models/VersionManifest.swift
@@ -0,0 +1,52 @@
+//
+//  VersionManifest.swift
+//  PCL.Mac
+//
+//  Created by 温迪 on 2025/12/2.
+//
+
+import Foundation
+import SwiftyJSON
+
+public struct VersionManifest {
+    public let latestRelease: String
+    public let latestSnapshot: String?
+    public let versions: [Version]
+    
+    public init(json: JSON) {
+        self.latestRelease = json["latest"]["release"].stringValue
+        self.latestSnapshot = json["latest"]["snapshot"].stringValue == latestRelease ? nil : json["latest"]["snapshot"].stringValue
+        self.versions = json["versions"].arrayValue.compactMap(Version.init(json:))
+    }
+    
+    public struct Version {
+        private static let dateFormatter: ISO8601DateFormatter = {
+            let formatter: ISO8601DateFormatter = .init()
+            formatter.timeZone = TimeZone(identifier: "Asia/Shanghai")
+            return formatter
+        }()
+        
+        public let id: String
+        public let type: String // TODO
+        public let url: URL
+        public let time: Date
+        public let releaseTime: Date
+        
+        fileprivate init?(json: JSON) {
+            guard let url = URL(string: json["url"].stringValue),
+                  let time = Self.dateFormatter.date(from: json["time"].stringValue),
+                  let releaseTime = Self.dateFormatter.date(from: json["releaseTime"].stringValue)
+            else { return nil }
+            self.id = json["id"].stringValue
+            self.type = json["type"].stringValue
+            self.url = url
+            self.time = time
+            self.releaseTime = releaseTime
+        }
+    }
+    
+    public func getIndex(_ id: String) -> Int {
+        guard let index = versions.firstIndex(where: { $0.id == id }) else { return -1 }
+        return versions.count - index
+    }
+}
