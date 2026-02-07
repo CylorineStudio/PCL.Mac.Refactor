334b8f2b0ba3767c2e46cd206c53abef6cd53996
AnemoFlower <yizhimcqiu@icloud.com>
2026-02-07 17:38:10 +0800

    fix

--- PCL.Mac.Core/Utils/LocaleUtils.swift
+++ PCL.Mac.Core/Utils/LocaleUtils.swift
@@ -9,19 +9,19 @@ import Foundation
 import CoreLocation
 
 public enum LocaleUtils {
-    private static var inChinaMainland: Bool?
+    @MainActor private static var inChinaMainland: Bool?
     
     /// 判断系统地区设置是否为中国大陆。
     public static func isSystemLocaleChinese() -> Bool {
         return Locale.current.identifier == "zh_CN"
     }
     
-    /// 判断当前设备的物理位置是否在中国大陆。
+    /// 根据当前网络的公网 IP 推断所在地是否为中国大陆（网络所在地），并非精确的设备物理位置。
     ///
-    /// 位置查询使用了 [CloudFlare API](https://www.cloudflare-cn.com/cdn-cgi/trace) ，**可能会包含港澳台区域**。
-    /// - Parameter strict: 查询失败时是否返回 `false`。
+    /// 查询使用了 [CloudFlare Trace](https://www.cloudflare-cn.com/cdn-cgi/trace) 基于出口 IP 进行地区判断，
+    /// 结果会受到 VPN / 代理 / 网络环境等影响，且**可能会包含港澳台区域**。
     public static func isInChinaMainland(strict: Bool = true, useCache: Bool = true) async -> Bool {
-        if let inChinaMainland, useCache {
+        if let inChinaMainland = await inChinaMainland, useCache {
             return inChinaMainland
         }
         do {
@@ -30,7 +30,9 @@ public enum LocaleUtils {
                 encoding: .utf8
             ).unwrap("解析字符串失败。")
             let inChinaMainland = response.contains("\nloc=CN\n")
-            Self.inChinaMainland = inChinaMainland
+            await MainActor.run {
+                Self.inChinaMainland = inChinaMainland
+            }
             return inChinaMainland
         } catch {
             err("获取地区失败：\(error.localizedDescription)")
