7e7383830d84e98aa261affb8878f10f3935feac
AnemoFlower <yizhimcqiu@icloud.com>
2026-05-04 15:39:20 +0800

    feat: 支持从环境变量设置 Secrets

--- PCL.Mac/App/Secrets.swift
+++ PCL.Mac/App/Secrets.swift
@@ -17,10 +17,18 @@ struct Secrets {
         do {
             let secretsURL = URLConstants.resourcesURL.appending(path: "Secrets.properties")
             let secrets = try PropertiesLoader.load(at: secretsURL)
-            self.curseforgeApiKey = secrets["CURSE_FORGE_API_KEY"]
+            self.curseforgeApiKey = Self.secret(key: "CURSE_FORGE_API_KEY", in: secrets)
         } catch {
             err("加载 Secrets.properties 失败：\(error.localizedDescription)")
             self.curseforgeApiKey = nil
         }
+    }
+    
+    private static func secret(key: String, in secrets: [String: String]) -> String? {
+        if let secret = ProcessInfo.processInfo.environment["PCLMAC_\(key)"] ?? secrets[key] {
+            return secret
+        }
+        warn("缺少 Secret 项 \(key)")
+        return nil
     }
 }
