78d603970f06281f5c3fe9477798de082de8a7ce
AnemoFlower <yizhimcqiu@icloud.com>
2026-05-04 15:48:47 +0800

    fix

--- PCL.Mac/App/Secrets.swift
+++ PCL.Mac/App/Secrets.swift
@@ -14,21 +14,25 @@ struct Secrets {
     public let curseforgeApiKey: String?
     
     private init() {
+        let secrets: [String: String]?
         do {
             let secretsURL = URLConstants.resourcesURL.appending(path: "Secrets.properties")
-            let secrets = try PropertiesLoader.load(at: secretsURL)
-            self.curseforgeApiKey = Self.secret(key: "CURSE_FORGE_API_KEY", in: secrets)
+            secrets = try PropertiesLoader.load(at: secretsURL)
         } catch {
             err("加载 Secrets.properties 失败：\(error.localizedDescription)")
-            self.curseforgeApiKey = nil
+            secrets = nil
         }
+        
+        curseforgeApiKey = Self.secret(key: "CURSE_FORGE_API_KEY", in: secrets)
     }
     
-    private static func secret(key: String, in secrets: [String: String]) -> String? {
-        if let secret = ProcessInfo.processInfo.environment["PCLMAC_\(key)"] ?? secrets[key] {
+    private static func secret(key: String, in secrets: [String: String]?) -> String? {
+        if let secret = ProcessInfo.processInfo.environment["PCLMAC_\(key)"] ?? secrets?[key] {
             return secret
         }
-        warn("缺少 Secret 项 \(key)")
+        if secrets != nil {
+            warn("缺少 Secret 项 \(key)")
+        }
         return nil
     }
 }
