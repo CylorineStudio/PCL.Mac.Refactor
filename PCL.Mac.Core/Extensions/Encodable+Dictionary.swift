2a8b1a52f9dfe67b6c7c24789a36a1f7bb8476e9
AnemoFlower <yizhimcqiu@icloud.com>
2026-05-21 15:04:10 +0800

    fix

--- PCL.Mac.Core/Extensions/Encodable+Dictionary.swift
+++ PCL.Mac.Core/Extensions/Encodable+Dictionary.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension Encodable {
-    func toDictionary() throws -> [String: Any] {
+    func toDictionary() throws -> [String: Any]? {
         let data = try JSONEncoder.shared.encode(self)
         let json = try JSONSerialization.jsonObject(with: data)
         return json as? [String: Any] ?? [:]
