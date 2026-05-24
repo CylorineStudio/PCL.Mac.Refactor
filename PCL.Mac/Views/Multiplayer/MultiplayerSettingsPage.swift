18de3c8133d508b7fdfdb832558bc6536d3c182f
AnemoFlower <yizhimcqiu@icloud.com>
2026-05-24 16:23:43 +0800

    chore: 移除自定义节点 UI 设置

--- PCL.Mac/Views/Multiplayer/MultiplayerSettingsPage.swift
+++ PCL.Mac/Views/Multiplayer/MultiplayerSettingsPage.swift
@@ -8,18 +8,9 @@
 import SwiftUI
 
 struct MultiplayerSettingsPage: View {
-    @State private var customPeer: String = LauncherConfig.shared.multiplayerCustomPeer ?? ""
-    
     var body: some View {
         CardContainer {
             MyCard("EasyTier 设置", foldable: false) {
-                configLine(label: "自定义节点") {
-                    MyTextField(text: $customPeer)
-                        .onChange(of: customPeer) { newValue in
-                            LauncherConfig.shared.multiplayerCustomPeer = newValue
-                        }
-                }
-                
                 HStack {
                     MyButton("删除 EasyTier", type: .red) {
                         EasyTierManager.shared.delete()
@@ -29,22 +20,7 @@ struct MultiplayerSettingsPage: View {
                     Spacer()
                 }
                 .frame(height: 35)
-                .padding(.top, 12)
             }
         }
-    }
-    
-    @ViewBuilder
-    private func configLine(label: String,  @ViewBuilder body: () -> some View) -> some View {
-        HStack(spacing: 20) {
-            MyText(label)
-                .frame(width: 120, alignment: .leading)
-            HStack {
-                Spacer(minLength: 0)
-                body()
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-        }
-        .padding(.horizontal, 6)
     }
 }
