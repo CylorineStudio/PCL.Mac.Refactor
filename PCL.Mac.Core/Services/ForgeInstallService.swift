e8698fcf9d86efb918bb7b16d9afa196689c8a3d
风花AnemoFlower <124678228+AnemoFlower@users.noreply.github.com>
2026-06-05 15:18:37 +0800

    fix: Forge 11.14.0.1296 以下版本安装器 URL 错误 (#197)

--- PCL.Mac.Core/Services/ForgeInstallService.swift
+++ PCL.Mac.Core/Services/ForgeInstallService.swift
@@ -128,7 +128,11 @@ public class ForgeInstallService {
     }
     
     internal func installerDownloadURL() -> URL {
-        return .init(string: "https://files.minecraftforge.net/maven/net/minecraftforge/forge/\(minecraftVersion)-\(version)/forge-\(minecraftVersion)-\(version)-installer.jar")!
+        let path = version.compare("11.14.0.1296", options: .numeric) != .orderedAscending
+        ? "net/minecraftforge/forge/\(minecraftVersion)-\(version)/forge-\(minecraftVersion)-\(version)-installer.jar"
+        : "net/minecraftforge/forge/\(minecraftVersion)-\(version)-\(minecraftVersion)/forge-\(minecraftVersion)-\(version)-\(minecraftVersion)-installer.jar"
+        
+        return .init(string: "https://files.minecraftforge.net/maven")!.appending(path: path)
     }
     
     private func makeValueDict() -> [String: String] {
