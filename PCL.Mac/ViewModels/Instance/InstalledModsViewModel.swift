8849ab297369aba245c4ae01dba1406707946535
AnemoFlower <yizhimcqiu@icloud.com>
2026-06-08 14:41:59 +0800

    feat: 原版实例处理

--- PCL.Mac/ViewModels/Instance/InstalledModsViewModel.swift
+++ PCL.Mac/ViewModels/Instance/InstalledModsViewModel.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 import Core
+import Combine
 
+@MainActor
 class InstalledModsViewModel: ObservableObject {
     @Published public var mods: [ModDisplayModel]?
+    @Published public var supportMods: Bool = true
+    @Published public var currentRepositoryId: UUID
+    private var cancellables: Set<AnyCancellable> = []
     private let instanceManager: InstanceManager
     private let id: String
     private let service: ModLoadService
@@ -17,11 +22,26 @@ class InstalledModsViewModel: ObservableObject {
     init(instanceManager: InstanceManager, id: String) {
         self.instanceManager = instanceManager
         self.id = id
+        self.currentRepositoryId = instanceManager.currentRepositoryId
         self.service = .init(remoteLookupService: .init(curseforgeClient: .init(apiKey: "")), cache: .shared)
+        
+        instanceManager.$currentRepositoryId
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.currentRepositoryId, on: self)
+            .store(in: &cancellables)
     }
     
     func load() async throws {
-        let modsDirectory = await instanceManager.currentRepository.instance(named: id)!.url.appending(path: "mods")
+        guard let instance = instanceManager.currentRepository.instance(named: id) else {
+            throw SimpleError("实例不存在。")
+        }
+        if instance.modLoader == nil {
+            await MainActor.run {
+                supportMods = false
+            }
+            return
+        }
+        let modsDirectory = instance.url.appending(path: "mods")
         let result: [ModDisplayModel] = try await service.loadMods(in: modsDirectory).values.lazy
             .map(ModDisplayModel.init)
             .sorted { $0.name.compare($1.name, options: .literal) == .orderedAscending }
--- PCL.Mac/Views/Launch/InstanceSettings/InstalledModsPage.swift
+++ PCL.Mac/Views/Launch/InstanceSettings/InstalledModsPage.swift
@@ -18,17 +18,49 @@ struct InstalledModsPage: View {
     }
     
     var body: some View {
-        CardContainer {
-            if let mods = viewModel.mods {
+        Group {
+            if !viewModel.supportMods {
                 MyCard(nil) {
-                    LazyVStack(spacing: 0) {
-                        ForEach(mods, id: \.id) { mod in
-                            ResourceListItem(resource: mod)
+                    VStack(spacing: 10) {
+                        MyText("该实例不可使用 Mod", size: 19, color: .color3)
+                        
+                        Rectangle()
+                            .fill(Color.color3)
+                            .frame(height: 2)
+                        
+                        MyText("你需要先安装 Forge、Fabric 等 Mod 加载器才能使用 Mod，请在安装实例时选择这些加载器。\n如果你已经安装了 Mod 加载器，那么你很可能选择了错误的实例，请点击实例选择按钮切换实例。")
+                            .padding(.vertical, 5)
+                        
+                        HStack(spacing: 20) {
+                            MyButton("转到下载页面", type: .highlight) {
+                                AppRouter.shared.setRoot(.download)
+                            }
+                            .frame(width: 140)
+                            MyButton("实例选择") {
+                                AppRouter.shared.setRoot(.launch)
+                                AppRouter.shared.append(.instanceList(repositoryId: viewModel.currentRepositoryId))
+                            }
+                            .frame(width: 140)
                         }
+                        .frame(height: 35)
+                    }
+                    .padding(10)
+                }
+                .padding(40)
+            } else {
+                CardContainer {
+                    if let mods = viewModel.mods {
+                        MyCard(nil) {
+                            LazyVStack(spacing: 0) {
+                                ForEach(mods, id: \.id) { mod in
+                                    ResourceListItem(resource: mod)
+                                }
+                            }
+                        }
+                    } else {
+                        MyLoading(viewModel: loadingVM)
                     }
                 }
-            } else {
-                MyLoading(viewModel: loadingVM)
             }
         }
         .task {
@@ -36,7 +68,7 @@ struct InstalledModsPage: View {
                 try await viewModel.load()
             } catch {
                 err("加载模组列表失败：\(error.localizedDescription)")
-                loadingVM.fail(with: error.localizedDescription)
+                loadingVM.fail(with: "加载模组列表失败：\(error.localizedDescription)")
             }
         }
     }
@@ -55,8 +87,7 @@ private struct ResourceListItem: View {
                 Group {
                     switch resource.icon {
                     case .resource(let imageResource): Image(imageResource).resizable()
-                    case .nsImage(let nsImage): Image(nsImage: nsImage).resizable()
-                            .interpolation(.none)
+                    case .nsImage(let nsImage): Image(nsImage: nsImage).resizable().interpolation(.none)
                     case .network(let url): NetworkImage(url: url)
                     }
                 }
